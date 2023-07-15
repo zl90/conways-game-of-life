9b4cc3fef34da8c61e7ddc16e0a81726876abfdd
zl90 <101400977+zl90@users.noreply.github.com>
2023-07-15 15:06:32 +1000

    Install webpack + add grid placeholder

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Colors = {
+    GRID_BACKGROUND: "#212730",
+};
+exports.default = Colors;
---
+++ src/colors.ts
@@ -0,0 +1,5 @@
+const Colors = {
+  GRID_BACKGROUND: "#212730",
+};
+
+export default Colors;
